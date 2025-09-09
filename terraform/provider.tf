bae06b4a697c08fc18d40bc92732afb24b88d825
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-09-09 18:49:14 +0100

    Update provider.tf
    Updated the version of aws .

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,8 +13,11 @@ locals {
 
 }
 
-provider "aws" {
-
-  region = local.region
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
+  }
 }
